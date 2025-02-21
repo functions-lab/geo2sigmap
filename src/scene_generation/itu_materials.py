725b54ade1a0ca63a4ab336e0db3d475ed4ec1f4
test <yl826@duke.edu>
2025-02-21 11:26:51 -0500

    Change the key name for each material to match up with Sionna/Mitsuba

--- src/scene_generation/itu_materials.py
+++ src/scene_generation/itu_materials.py
@@ -5,107 +5,108 @@ This module provides predefined material properties based on
 ITU-R Recommendation P.2040-2: "[Effects of building materials and structures
 on radiowave propagation above about 100 MHz](https://www.itu.int/dms_pubrec/itu-r/rec/p/R-REC-P.2040-3-202308-I!!PDF-E.pdf)".
 
-
+TODO: Verify the exact material names using by Mitsuba and Sionna. Especially how they named the low/high frequency glass and ceiling board materials.
+TODO: Check what is the exact colors using by Mitsuba/Sionna/Blender/Blosm for the materials.
 """
 
 ITU_MATERIALS = {
-    "vacuum": {
+    "mat-itu_vacuum": {
         "name": "Vacuum (\u2248Air)",
         "lower_freq_limit": 0.001e9,
         "upper_freq_limit": 100e9,
         "mitsuba_color": (0.1216, 0.4667, 0.7059),
     },
-    "concrete": {
+    "mat-itu_concrete": {
         "name": "Concrete",
         "lower_freq_limit": 1e9,
         "upper_freq_limit": 100e9,
         "mitsuba_color": (0.539479, 0.539479, 0.539480),
     },
-    "brick": {
+    "mat-itu_brick": {
         "name": "Brick",
         "lower_freq_limit": 1e9,
         "upper_freq_limit": 40e9,
         "mitsuba_color":(1.0000, 0.4980, 0.0549),
     },
-    "plasterboard": {
+    "mat-itu_plasterboard": {
         "name": "Plasterboard",
         "lower_freq_limit": 1e9,
         "upper_freq_limit": 100e9,
         "mitsuba_color":(1.0000, 0.7333, 0.4706),
     },
-    "wood": {
+    "mat-itu_wood": {
         "name": "Wood",
         "lower_freq_limit": 0.001e9,
         "upper_freq_limit": 100e9,
         "mitsuba_color": (0.043, 0.58, 0.184)
     },
-    "glass_low_freq": {
+    "mat-itu_glass_low_freq": {
         "name": "Glass(Low Frequency)",
         "lower_freq_limit": 0.1e9,
         "upper_freq_limit": 100e9,
         "mitsuba_color":(0.5961, 0.8745, 0.5412),
     },
-    "glass_high_freq": {
+    "mat-itu_glass_high_freq": {
         "name": "Glass(High Frequency)",
         "lower_freq_limit": 220e9,
         "upper_freq_limit": 450e9,
         "mitsuba_color":(0.8392, 0.1529, 0.1569),
     },
-    "ceiling_board_low_freq": {
+    "mat-itu_ceiling_board_low_freq": {
         "name": "Ceiling Board(Low Frequency)",
         "lower_freq_limit": 1e9,
         "upper_freq_limit": 100e9,
         "mitsuba_color":(1.0000, 0.5961, 0.5882),
     },
-    "ceiling_board_high_freq": {
+    "mat-itu_ceiling_board_high_freq": {
         "name": "Ceiling Board(High Frequency)",
         "lower_freq_limit": 220e9,
         "upper_freq_limit": 450e9,
         "mitsuba_color": (0.5804, 0.4039, 0.7412),
     },
-    "chipboard": {
+    "mat-itu_chipboard": {
         "name": "Chipboard",
         "lower_freq_limit": 1e9,
         "upper_freq_limit": 100e9,
         "mitsuba_color":(0.7725, 0.6902, 0.8353),
     },
-    "plywood": {
+    "mat-itu_plywood": {
         "name": "Plywood",
         "lower_freq_limit": 1e9,
         "upper_freq_limit": 40e9,
         "mitsuba_color":(0.5490, 0.3373, 0.2941),
     },
-    "marble": {
+    "mat-itu_marble": {
         "name": "Marble",
         "lower_freq_limit": 1e9,
         "upper_freq_limit": 60e9,
         "mitsuba_color": (0.701101, 0.644479, 0.485150),
     },
-    "floorboard": {
+    "mat-itu_floorboard": {
         "name": "Floorboard",
         "lower_freq_limit": 50e9,
         "upper_freq_limit": 100e9,
         "mitsuba_color":(0.8902, 0.4667, 0.7608),
     },
-    "metal": {
+    "itu_wet_groundmetal": {
         "name": "Metal",
         "lower_freq_limit": 1e9,
         "upper_freq_limit": 100e9,
         "mitsuba_color": (0.219526, 0.219526, 0.254152)
     },
-    "very_dry_ground": {
+    "itu_wet_groundvery_dry_ground": {
         "name": "Very Dry Ground",
         "lower_freq_limit": 1e9,
         "upper_freq_limit": 10e9,
         "mitsuba_color": (0.4980, 0.4980, 0.4980),
     },
-    "medium_dry_ground": {
+    "itu_wet_groundmedium_dry_ground": {
         "name": "Medium Dry Ground",
         "lower_freq_limit": 1e9,
         "upper_freq_limit": 10e9,
         "mitsuba_color":(0.7804, 0.7804, 0.7804),
     },
-    "wet_ground": {
+    "itu_wet_groundwet_ground": {
         "name": "Wet Ground",
         "lower_freq_limit": 1e9,
         "upper_freq_limit": 10e9,
